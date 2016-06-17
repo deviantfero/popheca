3c398d7ea819e4804f5dc518f199501306f4348a
deviantfero <fmorataya.04@gmail.com>
2016-06-17 01:18:08 -0600

    fix spacing between combobox and datepicker

--- src/gui/SearchGrid.java
+++ src/gui/SearchGrid.java
@@ -88,6 +88,8 @@ public class SearchGrid extends GridPane {
 		super.setMargin( this.txt_state, new Insets( 0, 0, 0, 90 ) );
 		super.setMargin( this.date_in, new Insets( 0, 0, 0, 90 ) );
 		super.setMargin( this.combo_amount, new Insets( 0, 90, 10, 0 ) );
+		super.setMargin( this.date_in, new Insets( 0, 0, 10, 0 ) );
+		super.setMargin( this.date_out, new Insets( 0, 0, 10, 0 ) );
 		super.add( this.top, 0, 0 );
 		if( this.translate )
 			this.translate_search();
