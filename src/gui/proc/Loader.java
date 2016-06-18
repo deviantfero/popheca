f50491cda5b0b18aac1d90fcc5325a26dc2751c6
deviantfero <fmorataya.04@gmail.com>
2016-06-18 13:23:07 -0600

    broaden stylesheet file search criteria

--- src/gui/proc/Loader.java
+++ src/gui/proc/Loader.java
@@ -6,10 +6,13 @@ import javafx.scene.Scene;
 public class Loader {
 	public static void loadCss( String filename, Scene applyScene ) {
 		File f = new File("bin/gui/styles/" + filename );
+		File f2 = new File("src/gui/styles/" + filename );
 		/*when it compiles it copies it to the bin folder
 		 *so it's necessary to have them on both dirs
 		 *make changes on src dir */
 		if( f.exists() )
+			applyScene.getStylesheets().add( "file://" + f.getAbsolutePath() );
+		else if( f2.exists() )
 			applyScene.getStylesheets().add( "file://" + f.getAbsolutePath() );
 		else
 			applyScene.getStylesheets().add("/gui/styles/" + filename );
