5ef0bc5a0bb25a10a97a95b56a0192f94e7a11fc
deviantfero <fmorataya.04@gmail.com>
2016-06-21 10:48:10 -0600

    add css filepath fix, new jar

--- src/gui/proc/Loader.java
+++ src/gui/proc/Loader.java
@@ -10,11 +10,17 @@ public class Loader {
 		/*when it compiles it copies it to the bin folder
 		 *so it's necessary to have them on both dirs
 		 *make changes on src dir */
-		if( f2.exists() )
-			applyScene.getStylesheets().add( "file:///" + f.getAbsolutePath() );
-		else if( f.exists() )
-			applyScene.getStylesheets().add( "file:///" + f.getAbsolutePath() );
-		else
+		if( f2.exists() ){
+			applyScene.getStylesheets().add( "file://" + f2.getAbsolutePath() );
+			System.out.println( "CSS::" + f2.getAbsolutePath() + " LOADED");
+		}
+		else if( f.exists() ){
+			applyScene.getStylesheets().add( "file://" + f.getAbsolutePath() );
+			System.out.println( "CSS::" + f.getAbsolutePath() + " LOADED");
+		}
+		else{
 			applyScene.getStylesheets().add("/gui/styles/" + filename );
+			System.out.println( "CSS::/gui/styles/" + filename + " LOADED");
+		}
 	}
 }
