096139ee4e60f581627759fe3b51bf9c985e14d7
Manuel <Manasan93@gmail.com>
2016-06-21 13:03:37 -0600

    Quick fix, working on windows, to test in other systems.

--- src/gui/proc/Loader.java
+++ src/gui/proc/Loader.java
@@ -5,8 +5,8 @@ import javafx.scene.Scene;
 
 public class Loader {
 	public static void loadCss( String filename, Scene applyScene ) {
-		File f = new File("bin/gui/styles/" + filename );
-		File f2 = new File("src/gui/styles/" + filename );
+		File f = new File("/bin/gui/styles/" + filename );
+		File f2 = new File("/src/gui/styles/" + filename );
 		/*when it compiles it copies it to the bin folder
 		 *so it's necessary to have them on both dirs
 		 *make changes on src dir */
